5947c75a8c808de6f084948cfb139c71ae008ea4
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-19 11:52:19 +0530

    Added permission request setup for maps page.

--- app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
+++ app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
@@ -14,37 +14,35 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DisasterMapFragment : Fragment(R.layout.disaster_map_fragment) {
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            when (isGranted) {
-                true -> setUpMap()
-                false -> showAlert()
-            }
-        }
+	private val requestPermissionLauncher =
+		registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+			when (isGranted) {
+				true -> setUpMap()
+				false -> showAlert()
+			}
+		}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> setUpMap()
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> showAlert()
-            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-    }
+		when {
+			ContextCompat.checkSelfPermission(
+				requireContext(),
+				Manifest.permission.ACCESS_FINE_LOCATION
+			) == PackageManager.PERMISSION_GRANTED -> setUpMap()
+			shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> showAlert()
+			else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+		}
+	}
 
-    private fun showAlert() = AlertDialog.Builder(requireContext()).let {
-        it.setTitle("Need permission(s)")
-        it.setMessage(
-            "Some permissions are required to open the map. Enable them in application settings."
-        )
-        it.setNeutralButton("OK", null)
-        it.create()
-    }.show()
+	private fun showAlert() = AlertDialog.Builder(requireContext()).let {
+		it.setTitle("Need permission(s)")
+		it.setMessage("Some permissions are required to open the map. Enable them in application settings.")
+		it.setNeutralButton("OK", null)
+		it.create()
+	}.show()
 
-    private fun setUpMap() {
-        Toast.makeText(activity, "Setting up map", Toast.LENGTH_SHORT).show()
-    }
+	private fun setUpMap() {
+		Toast.makeText(activity, "Setting up map", Toast.LENGTH_SHORT).show()
+	}
 }
