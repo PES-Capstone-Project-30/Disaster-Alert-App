b3a3e8073059cf31325def2cb5fc6c37cc0e0e11
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-20 11:39:13 +0530

    Updated google maps to show user location.

--- app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
+++ app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
@@ -1,15 +1,17 @@
 package com.jacob.disasteralertapp.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.jacob.disasteralertapp.R
 import com.jacob.disasteralertapp.databinding.DisasterMapFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,12 +22,28 @@ class DisasterMapFragment : Fragment(R.layout.disaster_map_fragment) {
     private val viewModel: DisasterMapViewModel by viewModels()
     private lateinit var googleMap: GoogleMap
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            when (isGranted) {
+                true -> setUpMap()
+                false -> showAlert()
+            }
+        }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        setUpMap()
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> setUpMap()
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> showAlert()
+            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
+    @Suppress("MissingPermission")
     private fun setUpMap() {
         val mapFragment =
             childFragmentManager.findFragmentById(binding.mapContainer.id) as SupportMapFragment
@@ -33,14 +51,16 @@ class DisasterMapFragment : Fragment(R.layout.disaster_map_fragment) {
         mapFragment.getMapAsync {
             googleMap = it
 
-            // Add a marker in Sydney and move the camera
-            val sydney = LatLng(-34.0, 151.0)
-            googleMap.addMarker(
-                MarkerOptions()
-                    .position(sydney)
-                    .title("Marker in Sydney")
-            )
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            googleMap.isMyLocationEnabled = true
         }
     }
+
+    private fun showAlert() = AlertDialog.Builder(requireContext()).let {
+        it.setTitle("Need permission(s)")
+        it.setMessage(
+            "Some permissions are required to open the map. Enable them in application settings."
+        )
+        it.setNeutralButton("OK", null)
+        it.create()
+    }.show()
 }
