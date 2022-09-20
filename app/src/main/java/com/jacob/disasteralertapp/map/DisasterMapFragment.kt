bebf0d4d667b27079c3e6354f544ddf6a6dc3740
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-20 10:56:00 +0530

    Removed open street maps with google maps.

--- app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
+++ app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapFragment.kt
@@ -1,78 +1,46 @@
 package com.jacob.disasteralertapp.map
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jacob.disasteralertapp.R
 import com.jacob.disasteralertapp.databinding.DisasterMapFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 @AndroidEntryPoint
 class DisasterMapFragment : Fragment(R.layout.disaster_map_fragment) {
     private val binding: DisasterMapFragmentBinding by viewBinding()
     private val viewModel: DisasterMapViewModel by viewModels()
-
-    private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            when (isGranted) {
-                true -> setUpMap()
-                false -> showAlert()
-            }
-        }
+    private lateinit var googleMap: GoogleMap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> setUpMap()
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> showAlert()
-            else -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
+        setUpMap()
     }
-
-    private fun showAlert() = AlertDialog.Builder(requireContext()).let {
-        it.setTitle("Need permission(s)")
-        it.setMessage(
-            "Some permissions are required to open the map. Enable them in application settings."
-        )
-        it.setNeutralButton("OK", null)
-        it.create()
-    }.show()
 
     private fun setUpMap() {
-        Toast.makeText(activity, "Setting up map", Toast.LENGTH_SHORT).show()
+        val mapFragment =
+            childFragmentManager.findFragmentById(binding.mapContainer.id) as SupportMapFragment
 
-        binding.map.setTileSource(TileSourceFactory.MAPNIK)
+        mapFragment.getMapAsync {
+            googleMap = it
 
-        val mLocationOverlay = MyLocationNewOverlay(
-            GpsMyLocationProvider(requireContext()),
-            binding.map
-        )
-        mLocationOverlay.enableMyLocation()
-        binding.map.overlays.add(mLocationOverlay)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.map.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        binding.map.onPause()
+            // Add a marker in Sydney and move the camera
+            val sydney = LatLng(-34.0, 151.0)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Sydney")
+            )
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
     }
 }
