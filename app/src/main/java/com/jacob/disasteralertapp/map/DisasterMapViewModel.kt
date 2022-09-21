879415e1dd1163f69a38fc630edaa22325ebd71e
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-21 15:08:21 +0530

    Updated DisasterMapViewModel to only get user location is a disaster is active.

--- app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapViewModel.kt
+++ app/src/main/java/com/jacob/disasteralertapp/map/DisasterMapViewModel.kt
@@ -3,22 +3,22 @@ package com.jacob.disasteralertapp.map
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jacob.disasteralertapp.common.AuthData
+import com.jacob.disasteralertapp.common.data.repositories.DisasterDataRepository
 import com.jacob.disasteralertapp.common.data.repositories.UserLocationRepository
 import com.jacob.disasteralertapp.common.models.LocationData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class DisasterMapViewModel @Inject constructor(
+    private val authData: AuthData,
     private val userLocationRepository: UserLocationRepository,
-    private val authData: AuthData
+    private val disasterDataRepository: DisasterDataRepository
 ) : ViewModel() {
-    private val isDisasterActive: Boolean
-        get() = true
-
     private val _locationDataFlow: MutableStateFlow<List<LocationData>> = MutableStateFlow(
         emptyList()
     )
@@ -33,12 +33,13 @@ class DisasterMapViewModel @Inject constructor(
         getLocationDetails()
     }
 
-    private fun getLocationDetails() {
-        if (!isDisasterActive) return
+    private fun getLocationDetails() = viewModelScope.launch {
+        val isDisasterActive =
+            disasterDataRepository.isDisasterActive(authData.currentUser!!.city)
+                .firstOrNull() ?: false
+        if (!isDisasterActive) return@launch
 
-        viewModelScope.launch {
-            userLocationRepository.getLocationForCity(authData.currentUser!!.city)
-                .collect(_locationDataFlow::emit)
-        }
+        userLocationRepository.getLocationForCity(authData.currentUser!!.city)
+            .collect(_locationDataFlow::emit)
     }
 }
