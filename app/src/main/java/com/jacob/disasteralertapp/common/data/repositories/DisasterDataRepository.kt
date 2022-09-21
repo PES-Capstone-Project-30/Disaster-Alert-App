c5a38bdf2a35746edb5a80252567a6d1d5d14484
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-21 12:10:40 +0530

    Added repository for getting disaster data.

---
+++ app/src/main/java/com/jacob/disasteralertapp/common/data/repositories/DisasterDataRepository.kt
@@ -0,0 +1,25 @@
+package com.jacob.disasteralertapp.common.data.repositories
+
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+class DisasterDataRepository @Inject constructor(
+    private val firebaseDatabase: FirebaseDatabase
+) {
+    fun isDisasterActive(city: String) = callbackFlow {
+        firebaseDatabase.getReference(city)
+            .child(DISASTER_DATA)
+            .child(IS_ACTIVE)
+            .get()
+            .addOnSuccessListener {
+                trySendBlocking(it.getValue(Boolean::class.java) ?: false)
+            }
+    }
+
+    companion object {
+        private const val DISASTER_DATA = "disaster_data"
+        private const val IS_ACTIVE = "is_active"
+    }
+}
