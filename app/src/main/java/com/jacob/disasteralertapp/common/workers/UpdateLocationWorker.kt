82e3cc89025fa988eaa6b4bca1ea32e1860a9152
Jacob Bosco <jacob.bosco3075@gmail.com>
2022-09-22 12:42:25 +0530

    Added logs and helper function for creation of UpdateLocationWorker.

--- app/src/main/java/com/jacob/disasteralertapp/common/workers/UpdateLocationWorker.kt
+++ app/src/main/java/com/jacob/disasteralertapp/common/workers/UpdateLocationWorker.kt
@@ -7,9 +7,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
 import androidx.hilt.work.HiltWorker
-import androidx.work.CoroutineWorker
-import androidx.work.WorkManager
-import androidx.work.WorkerParameters
+import androidx.work.*
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseUser
 import com.jacob.disasteralertapp.common.AuthData
@@ -20,6 +18,7 @@ import com.jacob.disasteralertapp.common.data.repositories.UsersRepository
 import com.jacob.disasteralertapp.common.models.*
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import java.time.Duration
 import kotlinx.coroutines.flow.firstOrNull
 import timber.log.Timber
 
@@ -38,23 +37,40 @@ class UpdateLocationWorker @AssistedInject constructor(
     override suspend fun doWork(): Result {
         return try {
             val firebaseUser = authData.getLoggedInFirebaseUser()
-            val currentUser = firebaseUser.getLoggedInUserDetails() ?: return Result.failure()
+            val baseUser = authData.currentUser ?: firebaseUser.getLoggedInUserDetails()
 
-            if (!isDisasterActive(currentUser)) {
+            if (baseUser == null) {
+                Timber.e("baseUser is null")
+                return Result.failure()
+            }
+
+            if (!isDisasterActive(baseUser)) {
                 WorkManager.getInstance(context).cancelAllWorkByTag(TAG)
+                Timber.w("Disaster is not active, cancelling work")
                 return Result.success()
             }
 
-            if (!isPermissionGranted()) return Result.failure()
+            if (!isPermissionGranted()) {
+                Timber.e("Location permission not granted")
+                return Result.failure()
+            }
 
             LocationServices.getFusedLocationProviderClient(context)
                 .lastLocation
                 .addOnCompleteListener { locationTask ->
-                    if (!locationTask.isSuccessful) return@addOnCompleteListener
-                    val location = locationTask.result ?: return@addOnCompleteListener
+                    if (!locationTask.isSuccessful) {
+                        Timber.e("Failed to get location")
+                        return@addOnCompleteListener
+                    }
 
-                    val locationData = location.getLocationData(currentUser)
-                    userLocationRepository.updateUserLocationDetails(currentUser, locationData)
+                    val location = locationTask.result
+                    if (location == null) {
+                        Timber.e("Location is null")
+                        return@addOnCompleteListener
+                    }
+
+                    val locationData = location.getLocationData(baseUser)
+                    userLocationRepository.updateUserLocationDetails(baseUser, locationData)
                 }
 
             Result.success()
@@ -85,10 +101,26 @@ class UpdateLocationWorker @AssistedInject constructor(
     private fun isPermissionGranted() = ContextCompat.checkSelfPermission(
         context,
         Manifest.permission.ACCESS_FINE_LOCATION
-    ) != PackageManager.PERMISSION_GRANTED
+    ) == PackageManager.PERMISSION_GRANTED
 
     companion object {
         private const val TAG = "UpdateLocationWorker"
         private const val DEFAULT_MIN_INTERVAL = 15L
+
+        fun createWorkerRequest(context: Context) {
+            val workRequest =
+                PeriodicWorkRequestBuilder<UpdateLocationWorker>(
+                    Duration.ofMinutes(DEFAULT_MIN_INTERVAL)
+                )
+                    .addTag(TAG)
+                    .build()
+
+            WorkManager.getInstance(context)
+                .enqueueUniquePeriodicWork(
+                    TAG,
+                    ExistingPeriodicWorkPolicy.KEEP,
+                    workRequest
+                )
+        }
     }
 }
