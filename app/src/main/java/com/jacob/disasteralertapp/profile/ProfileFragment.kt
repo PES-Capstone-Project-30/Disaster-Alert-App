5f700cd8ee5de85d2b6c3ba1dc35577631efb2cf
Lavanya-Y <lavanyamiliy@gmail.com>
2022-09-24 11:36:56 +0530

    For log out.

--- app/src/main/java/com/jacob/disasteralertapp/profile/ProfileFragment.kt
+++ app/src/main/java/com/jacob/disasteralertapp/profile/ProfileFragment.kt
@@ -4,29 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.jacob.disasteralertapp.R
+import com.jacob.disasteralertapp.databinding.ProfileFragmentBinding
+import com.jacob.disasteralertapp.databinding.UserRegistrationDetailsFragmentBinding
+import com.jacob.disasteralertapp.login.ui.registration.user.UserRegistrationDetailsViewModel
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(R.layout.profile_fragment) {
+    private val binding: ProfileFragmentBinding by viewBinding()
+    private val viewModel: ProfileViewModel by viewModels()
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private lateinit var viewModel: ProfileViewModel
+        binding.logOutButton.setOnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.profile_fragment, container, false)
-    }
+            findNavController().navigate(
+                ProfileFragmentDirections.actionProfileToLoginFragment()
+            )
+            viewModel.onLogOutBtnClicked()
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+        }
     }
 }
